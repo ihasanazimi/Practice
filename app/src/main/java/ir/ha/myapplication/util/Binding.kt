dc52c2870c5f6172ea2a741c0490aa40c12287df
hasan <h.azimi@rahkar.co>
2022-04-12 13:59:28 +0430

    ''modified

--- app/src/main/java/ir/ha/myapplication/util/Binding.kt
+++ app/src/main/java/ir/ha/myapplication/util/Binding.kt
@@ -1,16 +1,15 @@
 package ir.ha.myapplication.util
 
 import android.widget.ImageView
-import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import ir.ha.myapplication.R
 
 
 object Binding {
-    @BindingAdapter("image_loader")
-    fun loadImage(iv: ImageView, url: String?) {
-        Glide.with(iv.context).load(url).transition(DrawableTransitionOptions.withCrossFade())
-            .placeholder(R.mipmap.ic_launcher).into(iv)
-    }
+//    @BindingAdapter("image_loader")
+//    fun loadImage(iv: ImageView, url: String?) {
+//        Glide.with(iv.context).load(url).transition(DrawableTransitionOptions.withCrossFade())
+//            .placeholder(R.mipmap.ic_launcher).into(iv)
+//    }
 }
