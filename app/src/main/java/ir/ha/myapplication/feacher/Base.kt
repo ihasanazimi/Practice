9cf9fcbc383742464c73dde6dc6c5ede52ffc609
hasan <h.azimi@rahkar.co>
2022-05-01 22:10:56 +0430

    add binding functions

--- app/src/main/java/ir/ha/myapplication/feacher/Base.kt
+++ app/src/main/java/ir/ha/myapplication/feacher/Base.kt
@@ -6,43 +6,54 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import ir.ha.myapplication.R
 
-abstract class BaseFragment : Fragment() , BaseView {
+
+abstract class BaseFragment : Fragment(), BaseView {
     override val rootView: ViewGroup?
         get() = view as ViewGroup?
     override val viewContext: Context?
         get() = context
 
 
-
+    fun <T : ViewDataBinding?> getBinding(layout: Int, parent: ViewGroup): T {
+        return DataBindingUtil.inflate(LayoutInflater.from(requireContext()), layout, parent, false)
+    }
 }
-abstract class BaseActivity : AppCompatActivity() , BaseView {
+
+
+abstract class BaseActivity : AppCompatActivity(), BaseView {
     override val rootView: ViewGroup?
         get() = window.decorView.rootView as ViewGroup
     override val viewContext: Context?
         get() = this
 
-
+    fun <T : ViewDataBinding?> getBinding(layout: Int): T {
+        return DataBindingUtil.setContentView(this, layout)
+    }
 }
+
 
 interface BaseView {
 
-    val rootView : ViewGroup?
-    val viewContext : Context?
+    val rootView: ViewGroup?
+    val viewContext: Context?
 
-    fun setProgressIndicator(mustShow : Boolean){
+    fun setProgressIndicator(mustShow: Boolean) {
         rootView.let {
             viewContext.let { context ->
                 var loadingBar = it?.findViewById<View>(R.id.loadingView)
                 if (loadingBar == null && mustShow) {
-                    loadingBar = LayoutInflater.from(context).inflate(R.layout.loading_bar, it, false) as FrameLayout?
+                    loadingBar = LayoutInflater.from(context)
+                        .inflate(R.layout.loading_bar, it, false) as FrameLayout?
                     rootView?.addView(loadingBar)
                 }
-                rootView?.visibility = if (mustShow) View.VISIBLE else  View.GONE
+                rootView?.visibility = if (mustShow) View.VISIBLE else View.GONE
             }
         }
     }
