db3ac8aa7f77b9e9a4c1c98a23397c7e0bfea9bc
Hsn <h.azimi@rahkar.co>
2022-08-09 18:21:40 +0430

    add MyFacktory class to project

---
+++ app/src/main/java/ir/ha/dep/ui/httpSamples/MyFactory.kt
@@ -0,0 +1,13 @@
+package ir.ha.dep.ui.httpSamples
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ir.ha.dep.ui.App
+
+class MyFactory(app : App) : ViewModelProvider.AndroidViewModelFactory(app) {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return super.create(modelClass)
+    }
+
+}
