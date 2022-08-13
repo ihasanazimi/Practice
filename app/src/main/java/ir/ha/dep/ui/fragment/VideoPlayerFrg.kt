189beeb8e0172b5b637e3cb709f0bebdb5d5b50b
Hsn <h.azimi@rahkar.co>
2022-08-13 17:14:04 +0430

    add progressBar for videoPalyer Fragment

--- app/src/main/java/ir/ha/dep/ui/fragment/VideoPlayerFrg.kt
+++ app/src/main/java/ir/ha/dep/ui/fragment/VideoPlayerFrg.kt
@@ -8,6 +8,7 @@ import android.widget.MediaController
 import ir.ha.dep.R
 import ir.ha.dep.databinding.FragmentVideoPlayerBinding
 import ir.ha.dep.ui.BaseFragment
+import ir.ha.dep.utility.extentions.hide
 import ir.ha.dep.utility.extentions.showToast
 
 class VideoPlayerFrg :  BaseFragment() {
@@ -32,16 +33,17 @@ class VideoPlayerFrg :  BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.videoPlayer.setVideoPath("https://hajifirouz6.asset.aparat.com/aparat-video/41d9bd95cd3f6d419b4b9435f9b2ee9146709585-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImNiMTQyMTZiYmQ5MjU1YWM0ZGVjZGRjYmRiYTJmOTI1IiwiZXhwIjoxNjYwNDEyMDY1LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.lW-MHak7uNL66sk8OaoiRA4KnEIzlr_2JvIfRlGPQ_U")
+
         binding.videoPlayer.setOnPreparedListener {
             // set controller for video player
             MediaController(requireContext()).apply {
                 setMediaPlayer(binding.videoPlayer)
                 binding.videoPlayer.setMediaController(this)
                 setAnchorView(binding.videoPlayer)
+                binding.pb.hide()
             }
             it.start()
         }
-
 
         // when video is finished!
         binding.videoPlayer.setOnCompletionListener {
@@ -50,6 +52,7 @@ class VideoPlayerFrg :  BaseFragment() {
 
         binding.videoPlayer.setOnErrorListener { mp, what, extra ->
             showToast(requireContext(),"Error in Video When Downloading..")
+            binding.pb.hide()
             return@setOnErrorListener false
         }
     }
