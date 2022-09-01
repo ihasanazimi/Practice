e789be72418f28dc99633e774c2acf891366c54c
Hsn <h.azimi@rahkar.co>
2022-09-01 20:56:49 +0430

    add new util files and add new base Classes

--- app/src/main/java/ir/ha/dep/utility/Binding.kt
+++ app/src/main/java/ir/ha/dep/utility/Binding.kt
@@ -1,24 +1,24 @@
-//package ir.ha.myapplication.utility
-//
-//import android.widget.ImageView
-//import androidx.databinding.BindingAdapter
-//import com.bumptech.glide.Glide
-//import com.bumptech.glide.load.engine.DiskCacheStrategy
-//import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-//import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-//import ir.ha.myapplication.R
-//
-//
-//object Binding {
-//    @BindingAdapter("image_loader")
-//    fun loadImage(iv: ImageView, url: String?) {
-//        Glide
-//            .with(iv.context)
-//            .load(url)
-//            .transition(DrawableTransitionOptions.withCrossFade())
-//            .placeholder(R.mipmap.ic_launcher)
-//            .diskCacheStrategy(DiskCacheStrategy.ALL) // catch images
-//            .transform(RoundedCorners(10))  // corner radius
-//            .into(iv)
-//    }
-//}
+package ir.ha.dep.utility
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import ir.ha.dep.R
+
+
+object Binding {
+    @BindingAdapter("image_loader")
+    fun loadImage(iv: ImageView, url: String?) {
+        Glide
+            .with(iv.context)
+            .load(url)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .placeholder(R.mipmap.ic_launcher)
+            .diskCacheStrategy(DiskCacheStrategy.ALL) // catch images
+            .transform(RoundedCorners(10))  // corner radius
+            .into(iv)
+    }
+}
