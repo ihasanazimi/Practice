bb382544a8a449012ef63581f3873f9c5f3f0781
Hsn <h.azimi@rahkar.co>
2022-09-13 16:41:01 +0430

    change context filed to activity on IntentActions.java

--- app/src/main/java/ir/ha/dummy/utility/IntentActions.java
+++ app/src/main/java/ir/ha/dummy/utility/IntentActions.java
@@ -1,36 +1,34 @@
 package ir.ha.dummy.utility;
-
 import static ir.ha.dummy.utility.extentions.UtilExtKt.isAppAvailable;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.widget.Toast;
 import java.util.List;
-import ir.ha.dummy.R;
 
+import ir.ha.dummy.R;
 
 public class IntentActions {
 
-    private final Context context;
+    private final Activity activity;
 
-    public IntentActions(Context context){
-        this.context = context;
+    public IntentActions(Activity activity){
+        this.activity = activity;
     }
 
 
-
-    public void callPhoneNumber(Activity activity , String phoneNumber){
+    public void callPhoneNumber(String phoneNumber){
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:"+phoneNumber));
         activity.startActivity(intent);
     }
 
-    public void sendMessageToPhoneNumber(Activity activity , String phoneNumber , String msg){
+    public void sendMessageToPhoneNumber(String phoneNumber , String msg){
         Intent intentSMS = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+phoneNumber));
         intentSMS.putExtra("sms_body", msg);
         activity.startActivity(intentSMS);
@@ -39,12 +37,12 @@ public class IntentActions {
 
     public void openLinkedInPage(String linkedId) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("linkedin://add/%@" + linkedId));
-        final PackageManager packageManager = context.getPackageManager();
+        final PackageManager packageManager = activity.getPackageManager();
         @SuppressLint("QueryPermissionsNeeded") final List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
         if (list.isEmpty()) {
             intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.linkedin.com/profile/view?id=" + linkedId));
         }
-        context.startActivity(intent);
+        activity.startActivity(intent);
     }
 
 
@@ -55,7 +53,7 @@ public class IntentActions {
         intent.setData(Uri.parse("mailto:" + email));
         intent.putExtra(Intent.EXTRA_SUBJECT, R.string.app_name);
         intent.putExtra(Intent.EXTRA_TEXT   , "");
-        context.startActivity(Intent.createChooser(intent, "Send mail..."));
+        activity.startActivity(Intent.createChooser(intent, "Send mail..."));
     }
 
 
@@ -66,9 +64,9 @@ public class IntentActions {
         likeIng.setPackage("com.instagram.android");
 
         try {
-            context.startActivity(likeIng);
+            activity.startActivity(likeIng);
         } catch (ActivityNotFoundException e) {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/"+InstagramID)));
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/"+InstagramID)));
         }
     }
 
@@ -76,19 +74,15 @@ public class IntentActions {
     public void shareMessageToTelegram(String msg)
     {
         final String appName = "org.telegram.messenger";
-        final boolean isAppInstalled = isAppAvailable(context.getApplicationContext(), appName);
+        final boolean isAppInstalled = isAppAvailable(activity.getApplicationContext(), appName);
         if (isAppInstalled)
         {
             Intent myIntent = new Intent(Intent.ACTION_SEND);
             myIntent.setType("text/plain");
             myIntent.setPackage(appName);
             myIntent.putExtra(Intent.EXTRA_TEXT, msg);//
-            context.startActivity(Intent.createChooser(myIntent, "Share with"));
+            activity.startActivity(Intent.createChooser(myIntent, "Share with"));
         }
-        else Toast.makeText(context, "Telegram not Installed", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(activity, "Telegram not Installed", Toast.LENGTH_SHORT).show();
     }
-
-
-
-
 }
