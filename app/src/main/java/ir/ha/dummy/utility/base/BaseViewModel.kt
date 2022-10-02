25f18fa9bec39cfc8f2a18d8e4c56ecae0c414b6
Hsn <iHasan.azimi@gmail.com>
2022-10-02 12:25:56 +0330

    add clear error liveData function into base View model

--- app/src/main/java/ir/ha/dummy/utility/base/BaseViewModel.kt
+++ app/src/main/java/ir/ha/dummy/utility/base/BaseViewModel.kt
@@ -1,15 +1,25 @@
 package ir.ha.dummy.utility.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseViewModel : ViewModel() {
+
     open val composable = CompositeDisposable()
         get() = field
+
+    open val errorLiveData = MutableLiveData<ArrayList<String>>()
+        get() = field
+
+
+    // for clear data after change state
+    open fun clearErrorLiveData(){}
 
 
     override fun onCleared() {
         composable.clear()
+        clearErrorLiveData()
         super.onCleared()
     }
 }
