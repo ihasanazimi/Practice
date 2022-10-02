cc32f62d454b503918528274eee82b44f5080a9d
Hsn <iHasan.azimi@gmail.com>
2022-10-02 12:25:10 +0330

    change error liveData observer codes

--- app/src/main/java/ir/ha/dummy/utility/base/BaseFragmentByVM.kt
+++ app/src/main/java/ir/ha/dummy/utility/base/BaseFragmentByVM.kt
@@ -8,22 +8,17 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import ir.ha.dummy.utility.extentions.showToast
 
 abstract class BaseFragmentByVM<V : ViewDataBinding , VM : BaseViewModel> : Fragment() {
 
     private var _binding: V? = null
-    abstract var viewModel: VM
+    abstract val viewModel: VM
 
     val binding get() = _binding!!
 
     val mainHelper by lazy { (requireActivity()) }
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this)[viewModel.javaClass]
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
@@ -36,6 +31,10 @@ abstract class BaseFragmentByVM<V : ViewDataBinding , VM : BaseViewModel> : Frag
     }
 
     open fun registerObservers(){
+
+        viewModel.errorLiveData.observe(viewLifecycleOwner){
+            if (it.size != 0) showToast(requireContext(),it.first())
+        }
 
     }
 
