9f73b805352c3b382ff7c1e4a8be00de519602ee
Hasan Azimi <ihasan.azimi@gmail.com>
2023-01-16 18:10:46 +0330

    rename Card Fragment

--- app/src/main/java/ir/ha/practice/ui/bottomnav/fragments/card/CardFragment.kt
+++ app/src/main/java/ir/ha/practice/ui/bottomnav/fragments/card/CardFragment.kt
@@ -6,7 +6,7 @@ import ir.ha.practice.R
 import ir.ha.practice.databinding.SampleFrgCardBinding
 import ir.ha.practice.utility.base.BaseFragment
 
-class CardFrg : BaseFragment<SampleFrgCardBinding>() {
+class CardFragment : BaseFragment<SampleFrgCardBinding>() {
     override val layoutId: Int get() = R.layout.sample_frg_card
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
